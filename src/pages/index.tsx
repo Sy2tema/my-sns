99433a45813d40a189c584e6ab0882e18cf57969
Sy2tem4 <bood00142@gmail.com>
2023-05-26 20:33:40 +0900

    Add: Setup project base

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// nextjs는 12, antd는 4버전으로 맞추자
+const Home = () => {
+  return (
+    <React.StrictMode>
+      <div>Hello</div>
+    </React.StrictMode>
+  );
+}
+
+export default Home;
