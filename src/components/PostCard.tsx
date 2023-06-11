67ea4bf989cebdc834dfd2d8fafa254d1749c4d7
Sy2tem4 <bood00142@gmail.com>
2023-06-11 17:25:11 +0900

    Update: PostCard 컴포넌트 틀 구현
    - Popover 컴포넌트 내부에서 user.ownUser가 null이 아닐 경우 수정, 삭제 버튼이 보이도록 구현 예정입니다.

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,6 +1,35 @@
-const PostCard = () => {
+import { EllipsisOutlined, HeartOutlined, MessageOutlined, RetweetOutlined } from "@ant-design/icons";
+import { Button, Card, Popover } from "antd";
+
+const PostCard = ({ post }) => {
     return (
-        <div>PostCard</div>
+        <div>
+            <Card
+                cover={post.images[0] && <PostImages images={post.images} />}
+                actions={[
+                    <RetweetOutlined key="retweet" />,
+                    <HeartOutlined key="heart" />,
+                    <MessageOutlined key="comment" />,
+                    <Popover key="more" content={[
+                        <Button.Group>
+                            {ownUser && <Button>수정</Button>
+                            <Button>삭제</Button>}
+                            <Button>신고</Button>
+                        </Button.Group>
+                    ]}>
+                        <EllipsisOutlined />
+                    </Popover>
+                ]}
+            >
+                <Image />
+                <Content />
+                <Buttons>
+
+                </Buttons>
+            </Card>
+            <CommentForm />
+            <Comments />
+        </div>
     );
 }
 
