8ff78e31ead5e71dbe0fee3a8602f0088bccea03
Sy2tem4 <bood00142@gmail.com>
2023-06-22 21:12:15 +0900

    Update: Indecator 추가, style 분리
    ImageZoom/index.tsx:
    - Indecator 컴포넌트를 추가해서 이미지 확대시 몇번째 이미지인지 사진 아래쪽에 표시되도록 했습니다.
    - Slick 컴포넌트의 afterChange를 beforeChange으로 변경해 성능을 개선했습니다.
    
    ImagesZoom/styles.ts:
    - index에 있던 styled-component들을 따로 분리해 재사용성 및 가독성을 향상시켰습니다.
    - export를 통해 추후 다른 컴포넌트에서 ImagesZoom의 CSS설정을 재사용할 수 있도록 조치했습니다.

--- src/components/ImagesZoom/index.tsx
+++ src/components/ImagesZoom/index.tsx
@@ -1,57 +1,7 @@
-import { CloseOutlined } from '@ant-design/icons';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import Slick from 'react-slick';
-import styled, { createGlobalStyle } from 'styled-components';
-
-const Overlay = styled.div`
-    position: fixed;
-    z-index: 5000;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-`;
-const Header = styled.header`
-    height: 44px;
-    background: white;
-    position: relative;
-    padding: 0;
-    text-align: center;
-
-    & h1 {
-        margin: 0;
-        font-size: 17px;
-        color: #333;
-        line-height: 44px;
-    }
-`;
-const CloseBtn = styled(CloseOutlined)`
-    position: absolute;
-    right: 0;
-    top: 0;
-    padding: 15px;
-    line-height: 14px;
-    cursor: pointer;
-`;
-const SlickWrapper = styled.div`
-    height: calc(100% - 44px);
-    background: #090909;
-`;
-const ImgWrapper = styled.div`
-    padding: 32px;
-    text-align: center;
-
-    & img {
-        margin: 0 auto;
-        max-height: 750px;
-    }
-`;
-const Global = createGlobalStyle`
-    .slick-slide {
-        display: inline-block;
-    }
-`;
+import { CloseBtn, Global, Header, ImgWrapper, Indecator, Overlay, SlickWrapper } from './styles';
 
 const ImagesZoom = ({ images, onClose }) => {
     const [currentSlide, setCurrentSlide] = useState(0);
@@ -66,7 +16,7 @@ const ImagesZoom = ({ images, onClose }) => {
                 <div>
                     <Slick
                         initialSlide={0}
-                        afterChange={(slide) => setCurrentSlide(slide)}
+                        beforeChange={(slide) => setCurrentSlide(slide)}
                         infinite
                         arrows={false}
                         slidesToShow={1}
@@ -78,6 +28,15 @@ const ImagesZoom = ({ images, onClose }) => {
                             </ImgWrapper>
                         ))}
                     </Slick>
+                    <Indecator>
+                        <div>
+                            {currentSlide + 1}
+                            {' '}
+                            /
+                            {' '}
+                            {images.length}
+                        </div>
+                    </Indecator>
                 </div>
             </SlickWrapper>
         </Overlay>
