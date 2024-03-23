6a1596252e00d33a8b6bbba34c7b6c11d429a0a9
GunHyuk Lee <37396444+Sy2tema@users.noreply.github.com>
2024-03-23 17:07:16 +0900

    wip: SSR을 위해 백엔드 서버로 쿠키 전송
    쿠키 전달시 SameSite 및 Partitioned 속성 문제 등으로 인해 connect.sid가 백엔드로 전달되지 못하고 있는 문제가 있어 해결이 필요한 상태입니다.
    현재 조사해본 결과 브라우저 정책으로 인해 보안 요구사항이 강화되었기 때문으로 확인했습니다.
    https를 사용하는 등 보안 정책을 강화하게 되면 가상 머신에 올려놓은 백엔드와 쿠키 통신이 원활해질 것으로 예상중입니다.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,10 @@ import PostForm from '../components/PostForm';
 import PostCard from '../components/PostCard';
 import { LOAD_POST_REQUEST } from '../reducers/post/actionTypes';
 import { LOAD_MY_INFO_REQUEST } from '../reducers/user/actionTypes';
+import { PostData } from '../reducers/post/types';
 import wrapper from '../store/configureStore';
+import axios from 'axios';
+import { GetServerSidePropsContext } from 'next';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -44,12 +47,16 @@ const Home = () => {
   return (
     <AppLayout>
       {ownUser && <PostForm />}
-      {mainPosts.map((post) => <PostCard key={post.id} post={post} />)}
+      {mainPosts.map((post: PostData) => <PostCard key={post.id} post={post} />)}
     </AppLayout>
   );
 }
 
-export const getServerSideProps = wrapper.getServerSideProps((store) => async () => {
+// 이 부분은 프론트 서버에서 실행되기 때문에 브라우저에서 손댈 수 없다
+export const getServerSideProps = wrapper.getServerSideProps((store) => async (context: GetServerSidePropsContext) => {
+  const cookie = context.req ? context.req.headers.cookie : '';
+  axios.defaults.headers.Cookie = cookie || '';
+
   store.dispatch({
     type: LOAD_MY_INFO_REQUEST,
   });
